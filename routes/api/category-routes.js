61ffb27ff6d6b96bc6d05cb654a4dc7faf9b4171
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-02-28 17:01:59 -0500

    structure and models

---
+++ routes/api/category-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  }).then((data)=>{
+    res.json(data)
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {id: req.params.id},
+    include: [Product]
+  }).then((data)=>{
+    res.json(data)
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
